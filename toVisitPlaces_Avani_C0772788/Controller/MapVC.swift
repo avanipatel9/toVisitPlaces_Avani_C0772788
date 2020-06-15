89e6a38ff70e80b7fa6a14b07b51c48e92060945
Avani Patel <anpatel36@yahoo.in>
2020-06-15 17:36:36 -0400

    [AP] location authorization done

--- toVisitPlaces_Avani_C0772788/Controller/MapVC.swift
+++ toVisitPlaces_Avani_C0772788/Controller/MapVC.swift
@@ -7,24 +7,74 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var travelModeSegment: UISegmentedControl!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        checkLocationServices()
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func checkLocationServices() {
+        
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
     }
-    */
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            //startTrackingUserLocation()
+            break
+        case .denied:
+            //Show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            //show them alert letting them know what's up
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    @IBAction func travelModeSegment(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func zoomInBtnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func zoomOutBtnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func drawLocationBtnClick(_ sender: UIButton) {
+    }
+}
 
+extension MapVC: CLLocationManagerDelegate {
+    
+}
+
+extension MapVC: MKMapViewDelegate {
+    
 }
